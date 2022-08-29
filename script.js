625fc46105d7db3d12a33208e7b160f683aa9cb8
Nancy Chaudhry <nancych14@gmail.com>
2022-08-29 18:38:10 +0500

    Random color works

--- script.js
+++ script.js
@@ -2,11 +2,26 @@ const container = document.querySelector('#container');
 const price = document.querySelector('#size');
 const output = document.querySelector('.grid-size');
 const color = document.querySelector('#color');
+const reset = document.querySelector('#reset');
+const randomColor = document.querySelector('.random-color');
 
 output.textContent = size.value;
+let randomColorChoice = false;
 
+const randomizer = () => {
+    return (
+        '#' +
+        Math.floor(Math.random() * 16777215)
+            .toString(16)
+            .padStart(6, '0')
+            .toUpperCase()
+    );
+};
+
+// Resets the grid and creates new grid based on grid size
 function generateGrid(gridSize) {
     container.innerHTML = '';
+    randomColorChoice = false;
     container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
     for (let i = 0; i < gridSize * gridSize; i++) {
         const box = document.createElement('div');
@@ -14,23 +29,38 @@ function generateGrid(gridSize) {
         container.appendChild(box);
     }
 }
-// TODO setup selector to allow user to change the grid size from 16 to 100
+// Beginning grid 
+generateGrid(16);
 
+// Event listeners
+// Checks for random color selection and sets random color to true
+randomColor.addEventListener('click', () => {
+    randomColorChoice = true;
+    console.log(randomizer());
+})
+
+color.addEventListener('click', () => {
+    randomColorChoice = false;
+})
+
+// Checks for a change in grid sizen from range input and regenerates grid
 size.addEventListener('input', () => {
     let gridSize = size.value;
     output.textContent = gridSize;
     generateGrid(gridSize);
 });
 
-color.addEventListener('input', () => {
-    console.log(color.value);
+reset.addEventListener('click', () => {
+    let gridSize = size.value;
+    generateGrid(gridSize);
 })
 
-generateGrid(16)
-
-// Event listener
+// Checks for 'hover' over the grid and changes the color of each div
 container.addEventListener('mouseover', (e) => {
     if (e.target.classList.contains('square')) {
         e.target.style.backgroundColor = color.value;
+        if (randomColorChoice === true) {
+            e.target.style.backgroundColor = randomizer();
+        }
     }
 });
